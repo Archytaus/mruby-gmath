e7beba245c9411a983ea891ce00d1bdebe0168ec
Ryan Scott <atthealma@gmail.com>
2013-09-03 09:35:55 +0800

    Added the vec4 class to be initialised with the gem.

--- src/mrb_gmath.c
+++ src/mrb_gmath.c
@@ -1,5 +1,6 @@
 #include "mrb_gmath.h"
 #include "mrb_vec2.h"
+#include "mrb_vec4.h"
 #include "mrb_mat4.h"
 
 static struct RClass* mrb_gmath_class = NULL;
@@ -9,6 +10,7 @@ mrb_mruby_gmath_gem_init(mrb_state* mrb) {
   mrb_gmath_class = mrb_define_module(mrb, "GMath");
   
   init_vec2(mrb);
+  init_vec4(mrb);
   init_mat4(mrb);
 }
 
